0ae64e0b222e7837f8afc29f1091879fb31a9ee0
Wallery Tsybrov <tsybrov.wallery@yandex.ru>
2025-08-31 14:59:19 +0300

    31/08/2025

--- cats/views.py
+++ cats/views.py
@@ -9,6 +9,11 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
